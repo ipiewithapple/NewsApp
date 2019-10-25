6f8f513ab5a9106aa989c6ba6298465b7e9d3b22
ipiewithapple <43034061+ipiewithapple@users.noreply.github.com>
2019-10-25 10:55:53 +0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/js/index.js',
+  output: {
+    filename: 'js/bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: __dirname + '/src/index.html',
+      inject: 'body'
+    })
+  ],
+  devServer: {
+    contentBase: 'dist',
+    compress: true,
+    port: 9000,
+    open: true
+  }
+};
