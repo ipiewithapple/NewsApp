d330243fe5308ade4249fba6b6b7df351ac91f2e
iPie <ipiewithapple@yandex.ru>
2020-12-15 18:51:45 +0300

    fix

--- src/js/index.js
+++ src/js/index.js
@@ -13,42 +13,42 @@ const newsBlock = document.querySelector('#news-block');
 
 // Select
 document.addEventListener('DOMContentLoaded', function () {
-  var elems = document.querySelectorAll('select');
-  var instances = M.FormSelect.init(elems);
+    var elems = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(elems);
 });
-
 
 // Request
 
 const getRequest = (url, search, country, category) => {
-  axios.get(url, {
-    params: {
-      apiKey: '7c2789a2da3e49cf9ba7392e20e6f8a8',
-      country: country,
-      category: category,
-      q: search
-    }
-  })
-    .then(function (response) {
-      console.log(response.data.articles, response);
-      renderNews(response.data.articles);
-    })
-    .catch(function (error) {
-      console.log(error);
-    })
-}
+    axios
+        .get(url, {
+            params: {
+                apiKey: '7c2789a2da3e49cf9ba7392e20e6f8a8',
+                country: country,
+                category: category,
+                q: search,
+            },
+        })
+        .then(function (response) {
+            console.log(response.data.articles, response);
+            renderNews(response.data.articles);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+};
 
 // Render News
 
 const renderNews = (articles) => {
-  while (newsBlock.firstChild) {
-    newsBlock.removeChild(newsBlock.firstChild);
-  }
-  const fragment = document.createDocumentFragment();
-  articles.forEach(({ title, url, urlToImage, description }) => {
-    const card = document.createElement('div');
-    card.classList.add('col', 's12', 'm6', 'l6', 'xl4');
-    const cardContent = `
+    while (newsBlock.firstChild) {
+        newsBlock.removeChild(newsBlock.firstChild);
+    }
+    const fragment = document.createDocumentFragment();
+    articles.forEach(({ title, url, urlToImage, description }) => {
+        const card = document.createElement('div');
+        card.classList.add('col', 's12', 'm6', 'l6', 'xl4');
+        const cardContent = `
     <div class="card">
       <div class="card-image">
         <img src="${urlToImage}">
@@ -59,35 +59,35 @@ const renderNews = (articles) => {
       </div>
     </div>
     `;
-    card.insertAdjacentHTML('afterbegin', cardContent);
-    fragment.append(card);
-  });
-  newsBlock.append(fragment);
-}
+        card.insertAdjacentHTML('afterbegin', cardContent);
+        fragment.append(card);
+    });
+    newsBlock.append(fragment);
+};
 
 country.addEventListener('change', () => {
-  getRequest(countriesNews, null, country.value, category.value)
+    getRequest(countriesNews, null, country.value, category.value);
 });
 
 category.addEventListener('change', () => {
-  getRequest(countriesNews, null, country.value, category.value)
+    getRequest(countriesNews, null, country.value, category.value);
 });
 
 const search = () => {
-  if (!searchInput.value) {
-    invalidInput.style.display = 'block';
-  } else {
-    getRequest(everethingNews, searchInput.value);
-    invalidInput.style.display = 'none';
-    searchInput.value = '';
-  }
-}
+    if (!searchInput.value) {
+        invalidInput.style.display = 'block';
+    } else {
+        getRequest(everethingNews, searchInput.value);
+        invalidInput.style.display = 'none';
+        searchInput.value = '';
+    }
+};
 
 searchBtn.addEventListener('click', search);
-searchInput.addEventListener('keydown', evt => {
-  if (evt.keyCode === 13) {
-    search();
-  }
-})
+searchInput.addEventListener('keydown', (evt) => {
+    if (evt.keyCode === 13) {
+        search();
+    }
+});
 
 getRequest(countriesNews, null, country.value, category.value);
